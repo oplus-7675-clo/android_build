77f1010909febbefe21bb31a1309f62c09f6409e
Omkar Chandorkar <gotenksIN@aospa.co>
2025-08-26 16:36:06 +0900

    core: Stop appending AOSP BUILD_ID
    - Google Play Certification does not like it
    
    Change-Id: Ia582147d7fc565ef96dd95a7dd53552c7ffc5b45
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- core/build_id.mk
+++ core/build_id.mk
@@ -18,8 +18,4 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-BUILD_ID=CQ2A.260607.001
-
-
-
-BUILD_ID:=${BUILD_ID}-CP2A.260605.016
+BUILD_ID := CQ2A.260607.001
